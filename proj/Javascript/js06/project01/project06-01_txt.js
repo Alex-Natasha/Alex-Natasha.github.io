15d77d374e8d584b32e90f84a2b73a3137a9e268
Natasha <natasha.andreyko@stu.cvccworks.edu>
2025-05-01 11:54:05 -0400

    Add files via upload

---
+++ proj/Javascript/js06/project01/project06-01_txt.js
@@ -0,0 +1,26 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 6
+      Project 06-01
+
+      Project to validate a form used for setting up a new account
+      Author: natasha
+      Date:   
+
+      Filename: project06-01.js
+*/
+
+var submitButton = document.getElementById("submitButton");
+var pwd = document.getElementById("pwd");
+var pwd2 = document.getElementById("pwd2");
+
+submitButton.addEventListener("click", function(){
+      if(pwd.validity.patternMismatch){
+            pwd.setCustomValidity("Your password must be at least 8 characters with at least one letter and one number");
+      }else if(pwd.validity.patternMismatch !== pwd2.validity.patternMismatch){
+            pwd.setCustomValidity("Your password must match");
+      }else{
+            pwd.setCustomValidity("");
+      }
+}
+);
